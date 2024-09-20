7e1aad941cfb76083ae59bf8d1dd635251c5cfa4
Viktor Nespolo <nespolopeixoto@outlook.com>
2024-09-20 14:42:19 -0300

    created basic layout for the website and added the images

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../stylesheets/Button.css"
+
+export default function Button() {
+
+    return  (
+        <button>
+            Botão
+        </button>
+    )   
+}
---
+++ src/components/ButtonWrapper.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "./Button.js"
+import "../stylesheets/ButtonWrapper.css"
+
+export default function ButtonWrapper() {
+
+    return  (
+        <div className='button-wrapper'>
+            <Button />
+        </div>
+    )   
+}
