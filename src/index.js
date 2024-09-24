421ee7b83d0d36821feeb18ea3f802ce62ae5746
Viktor Nespolo <nespolopeixoto@outlook.com>
2024-09-24 15:17:02 -0300

    fix mobile interface, finnish style and functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './stylesheets/Button.css'
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
